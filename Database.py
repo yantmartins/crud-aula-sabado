7bef6d02cbee1b85f069c13a21e1187296fd396f
Yan <yantmartins@live.com>
2025-10-25 10:32:08 -0400

    up saturdaaay crud

--- Database.py
+++ Database.py
@@ -25,7 +25,51 @@ class Database:
             print(err)
 
         finally:
-            self.close_connection()                
+            self.close_connection()
+
+    def select(self):
+        self.connect()
+
+        try:
+            self.cursor.execute("SELECT * FROM cliente")
+            result = self.cursor.fetchall()
+            return result
+
+        except Exception as err:
+            print(err)
+
+        finally:
+            self.close_connection()
+
+    def select_by_id(self,id):
+        self.connect()
+
+        try:
+            self.cursor.execute(f"SELECT * FROM cliente WHERE id_cli = {id}")                       
+            dado = self.cursor.fetchone()
+            return dado
+
+        except Exception as err:
+            print(err)
+
+        finally:
+            self.close_connection()
+
+
+    def delete(self,id_cli):
+        self.connect()
+
+        try:
+            self.cursor.execute(f"DELETE FROM cliente WHERE id_cli = {id_cli} ")
+            self.conn.commit()
+            return True
+
+        except Exception as err:
+            print(err)
+
+        finally:
+            self.close_connection()        
+
 
     def close_connection(self):
         if self.conn.is_connected():
@@ -35,7 +79,28 @@ class Database:
 
 db = Database()
 
-dados = ("Emily","66655544423","67955338811","PA")
-cadastro = db.insert(dados)                
-if cadastro == True:
-    print("Cadastrado com sucesso!")
+# id_cli = int(input("Digite o id do cliente que voce quer deletar: "))
+
+# dados = ("Emily","66655544423","67955338811","PA")
+# inserir = db.insert(dados)                
+# if inserir == True:
+#     print("Cadastrado com sucesso!")
+
+# excluir = db.delete(id_cli)
+# if excluir == True:
+#     print("Excluido com sucesso")    
+
+# clientes = db.select()
+# for cli in clientes:
+#     print(f'ID: {cli[0]} | Nome: {cli[1]} | CPF: {cli[2]} | Fone: {cli[3]} | Cidade: {cli[4]}')
+
+cliente = db.select_by_id(1)
+if cliente:
+    print("Cliente encontrado:")
+    print(f"ID: {cliente[0]}")
+    print(f"Nome: {cliente[1]}")
+    print(f"CPF: {cliente[2]}")
+    print(f"Fone: {cliente[3]}")
+    print(f"Cidade: {cliente[4]}")
+else:
+    print("Cliente não encontrado.")        
